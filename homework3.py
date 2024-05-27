7a8417728b816a5e65a12dfe5497f989ec8037fb
iamIlyaGurov <iam.gurov@yandex.ru>
2024-05-27 23:36:40 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,10 @@
+name = 'Ilya'
+print(name, type(name))
+age = 27
+new_age = 27 + 13
+print(age, type(age))
+print('Name:', name)
+print('Age:', age)
+print('New Age:', new_age)
+is_student = True
+print('Is Student:', is_student)
